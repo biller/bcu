ba44e3191a2aac8db40194b771905910b2409da4
Juan Sorondo <juan@biller.uy>
2020-06-03 13:59:25 -0300

    Se agrega cache de cotizaciones en el filesystem local

--- src/Cotizaciones.php
+++ src/Cotizaciones.php
@@ -25,6 +25,11 @@ class Cotizaciones
             $fecha = self::obtenerUltimoCierre();
         }
 
+        $cache = self::cacheGet($fecha, $moneda, $grupo);
+        if ($cache) {
+            return $cache;
+        }
+
         $params = [
             'Entrada' => [
                 'FechaDesde' => $fecha,
@@ -37,11 +42,37 @@ class Cotizaciones
         $client = self::getSoapClient('awsbcucotizaciones');
         $response = $client->Execute($params);
 
-        return $response->Salida->datoscotizaciones->{'datoscotizaciones.dato'}->TCC;
+        $cotizacion = $response->Salida->datoscotizaciones->{'datoscotizaciones.dato'}->TCC;
+
+        self::cachePut($fecha, $moneda, $grupo, $cotizacion);
+
+        return $cotizacion;
     }
 
     private static function getSoapClient($ws)
     {
         return new SoapClient("https://cotizaciones.bcu.gub.uy/wscotizaciones/servlet/$ws?wsdl");
+    }
+
+    private static function cacheGet(string $fecha, int $moneda, int $grupo)
+    {
+        $path = __DIR__ . "/../cache/$fecha/$grupo/$moneda";
+
+        if (!file_exists($path)) {
+            return false;
+        }
+
+        return file_get_contents($path);
+    }
+
+    private static function cachePut(string $fecha, int $moneda, int $grupo, float $cotizacion)
+    {
+        $path = __DIR__ . "/../cache/$fecha/$grupo";
+
+        if (!is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+
+        file_put_contents("$path/$moneda", $cotizacion);
     }
 }
